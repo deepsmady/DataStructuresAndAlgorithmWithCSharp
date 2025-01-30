397857c33f547d1a8bf3d30b8f1add97f3661ddb
Deepak Madathil <deepakmadathil@gmail.com>
2025-01-30 09:20:31 +0530

    Project changes

---
+++ DSA_CSharp/BasicRecursion/BasicRecursion.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_TUF.BasicRecursion
+{
+    public class BasicRecursion
+    {
+        private static int _counter = 0;
+        public static void Print()
+        {
+            if (_counter == 3)
+            {
+                return;
+            }
+            Console.WriteLine(_counter);
+            _counter++;
+            Print();
+        }
+
+        public static void Print_N_Times(int i, int n)
+        {
+            if (i > n)
+            {
+                return;
+            }
+            Console.WriteLine("Deeps");
+            Print_N_Times(i + 1, n);
+        }
+
+        public static void Print_1ToN(int i, int n)
+        {
+            if (i > n)
+            {
+                return;
+            }
+            Console.WriteLine(i + " ");
+            Print_1ToN(i + 1, n);
+        }
+
+        public static void Print_1ToN_Backtracking(int i, int n)
+        {
+            if (i < 1)
+            {
+                return;
+            }
+            Print_1ToN_Backtracking(i - 1, n);
+            Console.WriteLine(i + " ");
+        }
+
+        public static void Print_NTo1(int i, int n)
+        {
+            if (i < 1)
+            {
+                return;
+            }
+            Console.WriteLine(i);
+            Print_NTo1(i - 1, n);
+        }
+
+        public static void Print_NTo1_Backtracking(int i, int n)
+        {
+            if (i > n)
+            {
+                return;
+            }
+            Print_NTo1_Backtracking(i + 1, n);
+            Console.WriteLine(i);
+        }
+
+        public static void SumOfN_Parameterized(int i, int sum)
+        {
+            if (i < 1)
+            {
+                Console.WriteLine("Sum : " + sum);
+                return ;
+            }
+            SumOfN_Parameterized(i - 1, sum + i);
+        }
+
+        public static int SumOfN_Functional(int i)
+        {
+            if(i == 0) return 0;
+            return i + SumOfN_Functional(i - 1);
+        }
+
+        public static int Factorial_Functional(int i)
+        {
+            if (i == 0) return 1;
+            return i * Factorial_Functional(i - 1);
+        }
+
+        public static void ReverseArray_Recursion(int i, int n, int[] array)
+        {
+            if(i >= n / 2)
+            {
+                return;
+            }
+            var temp = array[i];
+            array[i] = array[n-i-1];
+            array[n-i- 1] = temp;
+                        
+            ReverseArray_Recursion(i + 1, n , array);
+        }
+
+        public static bool IsStringPalindrome_Recursion(int i, string str)
+        {
+            if (i >= str.Length / 2) return true;
+            if (str[i] != str[str.Length - i - 1]) return false;
+            return IsStringPalindrome_Recursion(i + 1, str);
+        }
+
+        public static int Fibonacci_Recursion(int n)
+        {
+            if (n <= 1) return n;
+            //f(n) = f(n-1) + f(n-2) => (last + secondlast)
+            int last = Fibonacci_Recursion(n - 1);
+            int secondlast = Fibonacci_Recursion(n - 2);
+            return last + secondlast;
+        }
+
+    }
+}
---
+++ DSA_CSharp_Arrays/Problems.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_TUF_Arrays
+{
+    internal class Problems
+    {
+        /// <summary>
+        /// eg : { 2, 6, 5, 8, 11 } => Target = 14
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSumProblemUsingHashMap(int[] arr, int target)
+        {
+            //Dict to store element(key) and index(value)
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int complement = target - arr[i];
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { i, map[complement] };
+                }
+                if (!map.ContainsKey(arr[i]))
+                {
+                    map.Add(arr[i], i);
+                }
+
+            }
+            return new int[] { -1, -1 };
+        }
+
+        /// <summary>
+        /// eg : { 2, 6, 5, 8, 11 } => Target = 14
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSumProblemUsing2Pointers(int[] arr, int target)
+        {
+            //store the initial index to a temp dictionary
+            var arrayIndexMap = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrayIndexMap.Add(arr[i], i);
+            }
+            int left = 0;
+            int right = arr.Length - 1;
+            //Sort the array
+            arr = arr.OrderBy(x => x).ToArray();
+
+            //Move the left and right pointers towards right and left respectively and check the sum
+            //If the sum is less than target, move left p else move right p.
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                int sum = (arr[left] + arr[right]);
+                if (sum == target) return new int[] { arrayIndexMap[arr[left]], arrayIndexMap[arr[right]] };
+                if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+
+        public int MaxProfit(int[] prices)
+        {
+            int min = prices[0];
+            int maxProfit = 0;
+            for (int i = 1; i <= prices.Length - 1; i++)
+            {
+                int proOrLoss = prices[i] - min;
+                maxProfit = Math.Max(maxProfit, proOrLoss);
+                min = Math.Min(min, prices[i]);
+            }
+            return maxProfit;
+        }
+
+        /// <summary>
+        /// TC = O(N)
+        /// SC = O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool ContainsDuplicate1(int[] nums)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// TC = O(N)
+        /// SC = O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public bool ContainsDuplicate2(int[] nums)
+        {
+            HashSet<int> tempList = new HashSet<int>();
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                tempList.Add(nums[i]);
+            }
+            return tempList.Count < nums.Length;
+        }
+
+        /// <summary>
+        /// TC = O(n)+O(n)+O(n)=O(n)
+        /// SC = 3*O(N) = O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int n = nums.Length;
+            int[] output = new int[n];
+            int[] prefix = new int[n];
+            int[] suffix = new int[n];
+
+            prefix[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                prefix[i] = prefix[i - 1] * nums[i - 1];
+            }
+
+            suffix[n - 1] = 1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                suffix[i] = suffix[i + 1] * nums[i + 1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                output[i] = prefix[i] * suffix[i];
+            }
+
+            return output;
+        }
+
+        public int SubarrayWithMaximumSum_KidanesAlgorithm(int[] arr)
+        {
+            int sum = 0;
+            int maxSum = int.MinValue;
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                sum = sum + arr[i];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+
+                if (sum < 0)
+                {
+                    sum = 0;
+                }
+            }
+            return maxSum < 0 ? maxSum : 0;
+        }
+
+        /// <summary>
+        /// Most intuitive method
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int SubarrayWithMaximumProduct(int[] arr)
+        {
+            int maxProduct = int.MinValue;
+            int preffixProduct = 1;
+            int suffixProduct = 1;
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                if (preffixProduct == 0)
+                {
+                    preffixProduct = 1;
+                }
+                if (suffixProduct == 0)
+                {
+                    suffixProduct = 1;
+                }
+
+                preffixProduct = preffixProduct * arr[i];
+                suffixProduct = suffixProduct * arr[arr.Length - i - 1];
+                maxProduct = Math.Max(maxProduct, Math.Max(preffixProduct, suffixProduct));
+            }
+            return maxProduct;
+        }
+
+        public void NextPermutation(int[] arr)
+        {
+            int n = arr.Length;
+            int dipIndex = -1;
+
+            // Step 1: Find the first decreasing element (dipIndex)
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    dipIndex = i;
+                    break;
+                }
+            }
+
+            // If no dipIndex is found, reverse the entire array to get the smallest permutation
+            if (dipIndex == -1)
+            {
+                Array.Reverse(arr);
+                return;
+            }
+
+            // Step 2: Find the smallest element greater than arr[dipIndex] to the right
+            for (int i = n - 1; i > dipIndex; i--)
+            {
+                if (arr[i] > arr[dipIndex])
+                {
+                    // Swap arr[dipIndex] and arr[i]
+                    (arr[dipIndex], arr[i]) = (arr[i], arr[dipIndex]);
+                    break;
+                }
+            }
+
+            // Step 3: Reverse the suffix starting from dipIndex + 1
+            Array.Reverse(arr, dipIndex + 1, n - dipIndex - 1);
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] arr)
+        {
+            List<IList<int>> ans = new List<IList<int>>();
+            Array.Sort(arr);  // Sort the array first
+
+            for (int i = 0; i < arr.Length - 2; i++)  // Run till arr.Length - 2
+            {
+                if (i > 0 && arr[i] == arr[i - 1]) continue; // Skip duplicate values for `i`
+
+                int j = i + 1, k = arr.Length - 1;
+
+                while (j < k)
+                {
+                    int sum = arr[i] + arr[j] + arr[k];
+
+                    if (sum < 0)
+                    {
+                        j++;
+                    }
+                    else if (sum > 0)
+                    {
+                        k--;
+                    }
+                    else
+                    {
+                        ans.Add(new List<int> { arr[i], arr[j], arr[k] });
+
+                        // Move pointers to avoid duplicate values
+                        while (j < k && arr[j] == arr[j + 1]) j++;
+                        while (j < k && arr[k] == arr[k - 1]) k--;
+
+                        j++;
+                        k--;
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+
+        public static int MajorityElement_NBy2(int[] arr)
+        {
+            int ctr = 0;
+            int n = arr.Length;
+            int ele = arr[0];
+            for (int i = 0; i <= n - 1; i++)
+            {
+                if(ctr == 0)
+                {
+                    ele = arr[i];
+                    ctr = 1;
+                }
+                else if (arr[i] == ele)
+                {
+                    ctr++;
+                }
+                else
+                {
+                    ctr--;
+                }
+            }
+
+            int ctr2 = 0;
+            for(int i = 0; i <= n - 1; i++)
+            {
+                if(arr[i] == ele)
+                {
+                    ctr2++;
+                }
+            }
+
+            if (ctr2 > n / 2) return ele;
+            return -1;
+        }
+
+
+    }
+}
